668fc2e6f065db12d7f99a77b6bc1843e5fd5793
Alan Sparrow <git@freeflight.org.uk>
2022-07-25 15:05:01 +0100

    Add WWGC TPs

--- turnpoints.py
+++ turnpoints.py
@@ -2,6 +2,13 @@ import argparse
 import csv
 import sys
 
+WWGC_EXTRAS = [
+    ["East Farndon","EFN","UK","5227.517N","00056.783W","130.0m","1","","","","","Centre of village"],
+    ["Elkington Canal Crossing","ELX","UK","5223.550N","00105.533W","130.0m","1","","","","","A14 Bridge over canal"],
+    ["HB Finish Ring","HBX","UK","5226.300N","00102.467W","153.9m","4","","","","","Centre of Airfield"],
+    ["Naseby North","NBN","UK","5224.217N","00058.767W","187.0m","1","","","","","Bridge over A14"],
+    ["Walton","WLN","UK","5228.750N","00107.483W","137.0m","1","","","","","Centre of village"]]
+
 def parse_latlon(str):
     if str[-1] in "NS":
         deg = int(str[:2])
@@ -52,4 +59,7 @@ if __name__ == '__main__':
     writer = csv.writer(args.out_file)
     writer.writerow(["name","code","country","lat","lon","elev","style","rwydir","rwylen","freq","desc"])
     for tp in tps:
+        writer.writerow(tp)
+
+    for tp in WWGC_EXTRAS:
         writer.writerow(tp)
